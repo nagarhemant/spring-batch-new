9dcce065261d57977f3664043335772fbf65ea54
nischal <nischal.bhatt.87@gmail.com>
2022-03-09 09:02:17 +0800

    test

--- src/main/java/nischal/bhatt/service/SecondTasklet.java
+++ src/main/java/nischal/bhatt/service/SecondTasklet.java
@@ -11,7 +11,7 @@ public class SecondTasklet implements Tasklet{
 
 	@Override
 	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-		System.out.println("this is second tasklet step");
+		System.out.println("this is second tasklet step customized");
 		return RepeatStatus.FINISHED;
 	}
 
